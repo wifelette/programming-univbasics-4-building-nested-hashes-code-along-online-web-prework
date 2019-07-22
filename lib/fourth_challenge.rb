38666e2b8350f5da5601fc7ab34ba2cb211ced4e
Leah Silber <leahsilber@gmail.com>
2019-07-22 19:08:23 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,14 +8,14 @@ def fourth_challenge
         {
           name: "Benvolio",
           age: "17",
-          attitude: "worried" 
+          attitude: "worried"
           }, {
             name: "Mercutio",
             age: "18",
-            attitude: "hot-headed" 
+            attitude: "hot-headed"
           }
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -24,7 +24,7 @@ def fourth_challenge
         {
           name: "Steven",
           age: "30",
-          attitude: "confused" 
+          attitude: "confused"
           }, {
             name: "Nurse",
             age: "44",
@@ -34,6 +34,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
